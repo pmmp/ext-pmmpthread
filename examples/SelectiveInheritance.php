14da29006cc8f733e2dd39d03fde33dbb315c444
Dylan K. Taylor <dktapps@pmmp.io>
2023-11-21 16:15:44 +0000

    examples: further improvement to inheritance example

--- examples/SelectiveInheritance.php
+++ examples/SelectiveInheritance.php
@@ -16,8 +16,8 @@ use pmmp\thread\Thread;
 
 /*
  * When starting a Thread, many things must be copied from the parent thread's context. This includes:
- * - INI settings
- * - Code (classes, functions, constants, list of included files)
+ * - INI settings set by ini_set()
+ * - User-defined code (classes, functions, constants, list of included files)
  *
  * This process is extremely costly and bug-prone, and should be avoided wherever possible.
  *
@@ -27,6 +27,10 @@ use pmmp\thread\Thread;
  * - Thread::INHERIT_INI: Copies INI settings but no code. Use this if you've used ini_set() and want to make sure the settings are copied, and your code can't re-apply them inside the thread.
  * - Thread::INHERIT_ALL: Copies everything. Use in single-file scripts, or when you can't autoload parts of your code (slow, wastes memory)
  * - Everything else: Never, they are legacy leftovers and have no good use case
+ *
+ * NOTE: The following things are **always** available on threads, regardless of INHERIT_* options:
+ * - Built-in classes, functions and constants (provided by extensions or the PHP core)
+ * - Preloaded user-defined classes, functions and constants (https://www.php.net/manual/en/opcache.configuration.php#ini.opcache.preload)
  */
 
 class my_class{
@@ -43,14 +47,19 @@ ini_set('memory_limit', '123456789');
 class Selective extends Thread{
 	public function run() : void{
 		/* functions exist where Thread::INHERIT_FUNCTIONS is set */
-		echo "Function inherited: " . (function_exists("my_function") ? "yes" : "no") . "\n";
+		echo "User-defined function inherited: " . (function_exists("my_function") ? "yes" : "no") . "\n";
 		/* classes exist where Thread::INHERIT_CLASSES is set **BE CAREFUL** */
-		echo "Class inherited: " . (class_exists("my_class") ? "yes" : "no") . "\n";
+		echo "User-defined class inherited: " . (class_exists("my_class") ? "yes" : "no") . "\n";
 		/* constants exist where Thread::INHERIT_CONSTANTS is set */
-		echo "Constant inherited: " . (defined("my_constant") ? "yes" : "no") . "\n";
+		echo "User-defined constant inherited: " . (defined("my_constant") ? "yes" : "no") . "\n";
 
 		/* INI entries exist where Thread::INHERIT_INI is set */
-		echo "INI entries inherited: " . (ini_get('memory_limit') === '123456789' ? "yes" : "no") . "\n";
+		echo "ini_set() INI entries inherited: " . (ini_get('memory_limit') === '123456789' ? "yes" : "no") . "\n";
+
+		/* built-in classes, functions and constants are always available */
+		echo "Built-in classes available: " . (class_exists("stdClass") ? "yes" : "no") . "\n";
+		echo "Built-in functions available: " . (function_exists("var_dump") ? "yes" : "no") . "\n";
+		echo "Built-in constants available: " . (defined("PHP_VERSION") ? "yes" : "no") . "\n";
 	}
 }
 
